3210ab2edcf2faffce32f1adecb34c29f894c8ac
Ahmad Heshmati <amd.heshmati@gmail.com>
2024-09-06 16:31:51 -0600

    Displaying critic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,19 @@
+import { Badge } from "@chakra-ui/react";
+import React from "react";
+
+interface Props {
+  score: string;
+}
+
+const CriticScore = ({ score }: Props) => {
+  let scoreNum = parseInt(score);
+
+  let color = scoreNum > 75 ? "green" : scoreNum > 60 ? "yellow" : "";
+  return (
+    <Badge colorScheme={color} fontSize={"14px"} paddingX={2} borderRadius={"4px"}>
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
